3f52878b7db87f2ea438e98a860af09cd6bb43c5
Vicky2114 <vr6994056@gmail.com>
2023-09-27 16:26:58 +0530

    vcd

--- react/fitness-app/src/components/Footer.js
+++ react/fitness-app/src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import Logo from '../assets/images/l.png';
 
 const Footer = () => (
   <Box mt="80px" bgcolor=" #b7b7b7">
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '254px', height: '198px' }} />
+      <img src={Logo} alt="logo" style={{ width: '200px', height: '198px' }} />
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by CODECODER</Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">made by Vishal Rawat</Typography>
   </Box>
 );
 
